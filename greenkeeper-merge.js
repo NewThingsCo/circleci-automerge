03bc6b73be2b6cbbd4f045407191a9d5c4630058
Laura Vuorenoja <laura.vuorenoja@labofnew.com>
2017-08-01 14:56:00 +0300

    Merge greenkeeper pull request automatically if test succeeds.

---
+++ greenkeeper-merge.js
@@ -0,0 +1,58 @@
+import fetch from 'node-fetch'
+
+const apiFetch = (url, param) => fetch(url, param).then(res => res.json()).then(json => json)
+
+const apiToken = process.env.GH_TOKEN
+const branchName = process.env.CIRCLE_BRANCH
+const commitSha = process.env.CIRCLE_SHA1
+
+const print = msg => console.log(msg)
+
+if (!apiToken) {
+  print('GitHub API token variable ($GH_TOKEN) not defined, abort')
+} else if (!branchName) {
+  print('Branch variable ($CIRCLE_BRANCH) not defined, abort')
+} else if (!commitSha) {
+  print('Commit SHA variable ($CIRCLE_SHA1) not defined, abort')
+} else if (branchName.startsWith('greenkeeper/')) {
+  const fetchParam = {
+    headers: {
+      Authorization: `token ${apiToken}`
+    }
+  }
+  // Get open PRs for peeps
+  apiFetch(
+    'https://api.github.com/repos/LabOfNew/peeps/pulls?state=open',
+    fetchParam
+  ).then(json => {
+    const pullRequest = json.find(request => {
+      if (request.head.ref === branchName && request.head.sha === commitSha) {
+        return true
+      }
+      return false
+    })
+    if (pullRequest) {
+      print(`Merge greenkeeper PR ${pullRequest.number}`)
+      fetchParam.method = 'PUT'
+      fetchParam.body = JSON.stringify({sha: commitSha})
+      // Merge PR that this branch is part of
+      apiFetch(
+        `https://api.github.com/repos/LabOfNew/peeps/pulls/${pullRequest.number}/merge`,
+        fetchParam
+      ).then(putJson => {
+        print(putJson.message)
+        if (putJson.merged) {
+          fetchParam.method = 'DELETE'
+          fetchParam.body = ''
+          // If merge was successful, delete branch
+          apiFetch(
+            `https://api.github.com/repos/LabOfNew/peeps/git/refs/heads/${branchName}`,
+            fetchParam
+          )
+        }
+      })
+    } else {
+      print(`Aborting merge, unable to find PR for branch ${branchName}`)
+    }
+  })
+}
