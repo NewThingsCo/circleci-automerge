4735aaf4ec7f52ca982c2528965075aada6f7584
Niko Ruotsalainen <niko.ruotsalainen@newthings.co>
2018-06-13 13:49:19 +0300

    Revert "debug"
    This reverts commit 17cb886690614d1e9d6e1da66ff4d6081ec6ebcb.

--- bin/merge-pr.js
+++ bin/merge-pr.js
@@ -30,7 +30,6 @@ module.exports = () => {
 
   const findCurrentPullRequest = pullRequests => {
     const pullRequest = pullRequests.find(pr => pr.head.sha.startsWith(sha))
-    console.log(pullRequests, pullRequest)
     if (!pullRequest || !pullRequest.number) {
       console.error(chalk.red(`No pull request found with SHA ${sha}, abort.`))
       process.exit(1)
@@ -50,6 +49,6 @@ module.exports = () => {
     return mergePullRequest(pullRequestNumber)
   } else {
     console.log(chalk.yellow('CIRCLE_PULL_REQUEST environment variable not set! Fallback to GitHub API.'))
-    return fetchOpenPullRequests().then(findCurrentPullRequest)
+    return fetchOpenPullRequests().then(findCurrentPullRequest).then(mergePullRequest)
   }
 }
